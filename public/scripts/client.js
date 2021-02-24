1a0a632239ba12d0a54ae3a4bbe96b660b6555ad
Adam Marsala <adammarsala@hotmail.com>
2021-02-24 22:28:12 +0000

    Add ajax request to server /tweets with form data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -69,35 +69,66 @@ $(document).ready(function () {
       $('#tweet-container').append($tweet); 
     }
   };
+  
+  const ajaxRequest = function(formData) {
+    //${formData}
+    const url = `/tweets`;
+    console.log(formData);
+    
+    $.ajax(url, {
+      method: "POST",
+      data: formData,
+    })
+    .done(function(data) {
+      // do something with data you get from response
+      renderTweets(data);
+      console.log("DONE!");
+    })
+    .fail(function(error) {
+      console.log("Something went wrong :(", error);
+    })
+    .always(function() {
+      console.log("ajax request completed.");
+    })
+  };
+
+  // On form submit gives serialized query string to ajax request func, from <textarea name="tweet"> as tweet=query 
+  $('.new-tweet form').on('submit', function(event) {
+    event.preventDefault();
+    const formData = $(this).serialize();
+    ajaxRequest(formData);
+  });
+
+
 
 
   // Test / driver code (temporary)
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ];
 
-  renderTweets(data);
+  
   
 });
