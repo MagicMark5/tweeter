11c6838dfc1a96fc6cf87d5e4525ca53bf219ec8
Adam Marsala <adammarsala@hotmail.com>
2021-02-26 18:10:43 +0000

    Refactored time-converters.js into a much simpler and more reliable solution!

--- public/scripts/time-converters.js
+++ public/scripts/time-converters.js
@@ -1,40 +1,28 @@
-// corrects 11 minute lag in day change for UTC near midnight.
-const correctDay = (day, hours, minutes) => {
-  if (hours === 23 && minutes >= 49) {
-    return day - 1;
-  }
-  return day;
-}
-
-// Returns an object with number values for date properties on the date object
-const getTimeData = (date) => {  
-  const timeData = {
-    year: date.getFullYear(), 
-    month: date.getMonth() + 1, 
-    hour: date.getHours(), 
-    minute: date.getMinutes(), 
-    second: date.getSeconds(), 
-    day: correctDay(date.getUTCDate(), date.getHours(), date.getMinutes())
-  };
-
-  return timeData; 
-}; 
-
-
 // takes in a past timestamp and returns "x units of time ago"
 const convertTimeToRelative = (timestamp) => {
 
   const timeOrder = ["year", "month", "day", "hour", "minute", "second"];
 
-  const now = getTimeData(new Date(Date.now()));
-  const timeOfTweet = getTimeData(new Date(timestamp));
+  const msTime = {
+    year: 1000 * 60 * 60 * 24 * 31 * 365, // approximate
+    month: 1000 * 60 * 60 * 24 * 31, // approximate
+    day: 1000 * 60 * 60 * 24,
+    hour: 1000 * 60 * 60, 
+    minute: 1000 * 60, 
+    second: 1000,
+  };
 
-  // loop through key names of timeOrder and the first inequality will be returned as relative time difference
+  // Time difference
+  const tweetTimeElapsed = new Date() - timestamp;
+  
+  //loop through key names of timeOrder and the first inequality will be returned as relative time difference
     for (let time of timeOrder) {
-      if (now[time] !== timeOfTweet[time]) {
-        let plural = now[time] - timeOfTweet[time] === 1 ? "" : "s";
-        return `${now[time] - timeOfTweet[time]} ${time += plural} ago`;
+      if (tweetTimeElapsed >= msTime[time]) {
+        let howManyUnitsOfTime = tweetTimeElapsed / msTime[time];
+        let plural = Math.floor(howManyUnitsOfTime) === 1 ? "" : "s";
+        return `${Math.floor(howManyUnitsOfTime)} ${time += plural} ago`;
       }
     }
+  // If elapsed time is less than a second return this...
   return `Just a moment ago.`;
 };
